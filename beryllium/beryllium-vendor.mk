0fce36e90ef6e9a26e0336274da3534104740133
LuK1337 <priv.luk@gmail.com>
2020-09-19 15:26:35 +0200

    beryllium: Regen vintf manifest fragments

--- beryllium/beryllium-vendor.mk
+++ beryllium/beryllium-vendor.mk
@@ -342,10 +342,8 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/beryllium/proprietary/vendor/lib64/vendor.qti.hardware.cacert@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.cacert@1.0.so \
     vendor/xiaomi/beryllium/proprietary/vendor/lib64/vendor.qti.hardware.fm@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.fm@1.0.so
 
-vintf_fragments += \
-    vendor/xiaomi/beryllium/proprietary/vendor/etc/vintf/manifest/android.hardware.gnss@2.0-service-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/vintf/manifest/android.hardware.gnss@2.0-service-qti.xml \
-    vendor/xiaomi/beryllium/proprietary/vendor/etc/vintf/manifest/vendor.qti.gnss@3.0-service.xml:$(TARGET_COPY_OUT_VENDOR)/etc/vintf/manifest/vendor.qti.gnss@3.0-service.xml
-
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.fm@1.0 \
-    CACertService
+    CACertService \
+    android.hardware.gnss@2.0-service-qti \
+    vendor.qti.gnss@3.0-service
